b3194d2051c15a5267991c2b6dda9f17d1ab8383
Abhilash <abhilash.gaddam@antra.com>
2021-07-09 18:56:05 -0400

    Initial commit

---
+++ Infrastructure/Services/MovieService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using ApplicationCore.Models;
+using ApplicationCore.ServiceInterfaces;
+
+namespace Infrastructure.Services
+{
+    public class MovieService : IMovieService
+    {
+        public List<MovieCardResponseModel> GetTopRevenueMovies()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MovieService2 : IMovieService
+    {
+        public List<MovieCardResponseModel> GetTopRevenueMovies()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+// DI
+
+// method(int x, MovieService service)
+// var service =  new MovieService();
+// method(4,service )
